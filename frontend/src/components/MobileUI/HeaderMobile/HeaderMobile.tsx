f29ede33b1c70c5772ba7f19a1b80a4893595aa6
Edin Mavric <edinmavric10@gmail.com>
2023-06-04 20:26:55 +0200

    Added working search news logic for Mobile users

--- frontend/src/components/MobileUI/HeaderMobile/HeaderMobile.tsx
+++ frontend/src/components/MobileUI/HeaderMobile/HeaderMobile.tsx
@@ -1,7 +1,37 @@
 import './HeaderMobile.css';
+import { useState } from 'react';
+import { RxHamburgerMenu, RxAvatar } from 'react-icons/rx';
+import { VscChromeClose } from 'react-icons/vsc';
+import HeaderMobileNavLink from './HeaderMobileNavLink';
 
 const HeaderMobile = () => {
-  return <header>Header Mobile</header>;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(prevState => !prevState);
+  };
+
+  return (
+    <header className="Header-Mobile">
+      <RxAvatar />
+      <nav
+        className={`${isDropdownOpen ? 'open' : ''}`}
+        onClick={toggleDropdown}
+      >
+        {isDropdownOpen ? <VscChromeClose /> : <RxHamburgerMenu />}
+        {isDropdownOpen && (
+          <div>
+            <HeaderMobileNavLink link="/" textContent="Vesti" />
+            <HeaderMobileNavLink link="/tourism" textContent="Turizam" />
+            <HeaderMobileNavLink
+              link="/report-a-problem"
+              textContent="Prijavi Problem"
+            />
+          </div>
+        )}
+      </nav>
+    </header>
+  );
 };
 
 export default HeaderMobile;
--- frontend/src/components/MobileUI/NewsMobile/NewsMobile.tsx
+++ frontend/src/components/MobileUI/NewsMobile/NewsMobile.tsx
@@ -1,9 +1,45 @@
-import './NewsMobile.css'
+import './NewsMobile.css';
+import { useEffect, useState } from 'react';
+import SecondaryNewsMobile from './HelperNewsMobile/SecondaryNewsMobile';
+import { NewsItems } from './HelperNewsMobile/NewsItemMobile';
 
-const NewsMobile = () => {
+const NewsMobile = ({ searchResults }: { searchResults: NewsItems[] }) => {
+  const [newsItems, setNewsItems] = useState<NewsItems[]>(searchResults);
+  const [selectedNews, setSelectedNews] = useState<NewsItems | null>(null);
+
+  const handleNewsItemClick = (news: NewsItems) => {
+    setSelectedNews(news);
+  };
+
+  useEffect(() => {
+    fetch('https://api.mojnovipazar.info/news/')
+      .then(response => response.json())
+      .then(data => setNewsItems(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    if (newsItems.length > 0) {
+      setSelectedNews(newsItems[0]);
+    }
+  }, [newsItems]);
+
+  useEffect(() => {
+    if (searchResults.length > 0) {
+      setNewsItems(searchResults);
+      setSelectedNews(searchResults[0]);
+    }
+  }, [searchResults]);
+
   return (
-    <div>News Mobile</div>
-  )
-}
+    <div className="News-Secondary-Mobile">
+      <SecondaryNewsMobile
+        newsItems={newsItems}
+        onNewsItemClick={handleNewsItemClick}
+        selectedNews={selectedNews}
+      />
+    </div>
+  );
+};
 
-export default NewsMobile
+export default NewsMobile;
