5e04f94529703b90efaa74163d7f377d6cb911c8
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-06-08 21:16:45 +0200

    desktop ui redesign

--- frontend/src/components/DesktopUI/News/News.tsx
+++ frontend/src/components/DesktopUI/News/News.tsx
@@ -1,44 +1,44 @@
-import './News.css';
-import MainNews from './HelperNews/MainNews';
-import SecondaryNews from './HelperNews/SecondaryNews';
-import { useEffect, useState } from 'react';
-import { NewsItems } from './HelperNews/NewsItem';
+import "./News.scss";
+import MainNews from "./HelperNews/MainNews";
+import SecondaryNews from "./HelperNews/SecondaryNews";
+import { useEffect, useState } from "react";
+import { NewsItems } from "./HelperNews/NewsItem";
 
 const News = () => {
-  const [newsItems, setNewsItems] = useState<NewsItems[]>([]);
-  const [selectedNews, setSelectedNews] = useState<NewsItems | null>(null);
+    const [newsItems, setNewsItems] = useState<NewsItems[]>([]);
+    const [selectedNews, setSelectedNews] = useState<NewsItems | null>(null);
 
-  const handleNewsItemClick = (news: NewsItems) => {
-    setSelectedNews(news);
-  };
+    const handleNewsItemClick = (news: NewsItems) => {
+        setSelectedNews(news);
+    };
 
-  useEffect(() => {
-    fetch('https://api.mojnovipazar.info/news/')
-      .then(response => response.json())
-      .then(data => setNewsItems(data))
-      .catch(error => console.log(error));
-  }, []);
+    useEffect(() => {
+        fetch("https://api.mojnovipazar.info/news/")
+            .then((response) => response.json())
+            .then((data) => setNewsItems(data))
+            .catch((error) => console.log(error));
+    }, []);
 
-  useEffect(() => {
-    if (newsItems.length > 0) {
-      setSelectedNews(newsItems[0]);
-    }
-  }, [newsItems]);
+    useEffect(() => {
+        if (newsItems.length > 0) {
+            setSelectedNews(newsItems[0]);
+        }
+    }, [newsItems]);
 
-  return (
-    <div className="News">
-      <div className="News-Main-News">
-        {selectedNews && <MainNews selectedNews={selectedNews} />}
-      </div>
-      <div className="News-Secondary-News">
-        <SecondaryNews
-          newsItems={newsItems}
-          onNewsItemClick={handleNewsItemClick}
-          selectedNews={selectedNews}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="News">
+            <div className="News-Main-News">
+                {selectedNews && <MainNews selectedNews={selectedNews} />}
+            </div>
+            <div className="News-Secondary-News">
+                <SecondaryNews
+                    newsItems={newsItems}
+                    onNewsItemClick={handleNewsItemClick}
+                    selectedNews={selectedNews}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default News;
