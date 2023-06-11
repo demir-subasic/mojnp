777629e79dacd9a14e10313c5f4330e81cc3e95d
Edin Mavric <edinmavric10@gmail.com>
2023-06-11 10:52:36 +0200

    Added tourism page Route's, nothing else started

--- frontend/src/components/DesktopUI/Header/Header.tsx
+++ frontend/src/components/DesktopUI/Header/Header.tsx
@@ -1,27 +1,27 @@
-import "./Header.scss";
-import HeaderNavLink from "./HeaderNavLink";
+import './Header.scss';
+import HeaderNavLink from './HeaderNavLink';
 
 const Header = () => {
-    return (
-        <header className="Header-Desktop">
-            <div>
-                <h1>
-                    MojNP<span>.</span>
-                </h1>
-            </div>
-            <nav>
-                <HeaderNavLink link="/" textContent="News" />
-                <HeaderNavLink link="/tourism" textContent="Tourism" />
-                <HeaderNavLink link="/report-a-problem" textContent="Alert" />
-            </nav>
-            <div className="buttons">
-                <div className="buttons-container">
-                    <button>Log in</button>
-                    <button>Sign up</button>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="Header-Desktop">
+      <div>
+        <h1>
+          MojNP<span>.</span>
+        </h1>
+      </div>
+      <nav>
+        <HeaderNavLink link="/" textContent="News" />
+        <HeaderNavLink link="/tourism" textContent="Tourism" />
+        <HeaderNavLink link="/report-a-problem" textContent="Alert" />
+      </nav>
+      <div className="buttons">
+        <div className="buttons-container">
+          <button>Log in</button>
+          <button>Sign up</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- frontend/src/components/DesktopUI/News/HelperNews/MainNews.tsx
+++ frontend/src/components/DesktopUI/News/HelperNews/MainNews.tsx
@@ -1,33 +1,31 @@
-import "./HelperNews.scss";
-import { AiOutlineCalendar } from "react-icons/ai";
-import { NewsItems } from "./NewsItem";
+import './HelperNews.scss';
+import { AiOutlineCalendar } from 'react-icons/ai';
+import { NewsItems } from './NewsItem';
 
 interface MainNewsProps {
-    selectedNews: NewsItems;
+  selectedNews: NewsItems;
 }
 
 const MainNews: React.FC<MainNewsProps> = ({ selectedNews }) => {
-    const { title, image, published, content, author } = selectedNews;
+  const { title, image, published, content, author } = selectedNews;
 
-    return (
-        <div className="News-Main">
-            <div
-                className="News-Main__Image"
-                style={{ backgroundImage: `url(${image})` }}
-            ></div>
-            <div className="News-Main__title">
-                <h2>{title}</h2>
-                <p>
-                    <AiOutlineCalendar />
-                    {published}
-                </p>
-            </div>
-            <p className="paragraph">{content}</p>
-            <p style={{ marginBottom: "0", color: "gray" }}>
-                Preuzeto: {author}
-            </p>
-        </div>
-    );
+  return (
+    <div className="News-Main">
+      <div
+        className="News-Main__Image"
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+      <div className="News-Main__title">
+        <h2>{title}</h2>
+        <p>
+          <AiOutlineCalendar />
+          {published}
+        </p>
+      </div>
+      <p className="paragraph">{content}</p>
+      <p style={{ marginBottom: '0', color: 'gray' }}>Preuzeto: {author}</p>
+    </div>
+  );
 };
 
 export default MainNews;
